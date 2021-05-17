8903c50ff672f34d5298c34ff3d17155e79b29db
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-05-17 17:24:57 +0100

    feat: Conditional Content && trick

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -23,9 +23,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={saveExpenseFilterYear}
         />
-        {filteredExpenses.length === 0 ? (
-          <p> No expenses found.</p>
-        ) : (
+        {filteredExpenses.length === 0 && <p> No expenses found.</p>}
+        {filteredExpenses.length > 0 &&
           filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
@@ -34,7 +33,7 @@ const Expenses = (props) => {
             date={expense.date}
           />
         ))
-        )}
+        }
       </Card>
     </div>
   );
