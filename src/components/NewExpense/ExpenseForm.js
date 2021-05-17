6ab8c3d2df855774cb827ac330a978df70039b4e
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-05-17 18:54:49 +0100

    feat: conditional content Practice

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -20,7 +20,6 @@ const ExpenseForm = (props) => {
   const submitHandler = (event) => {
     event.preventDefault();
 
-
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
@@ -67,6 +66,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
